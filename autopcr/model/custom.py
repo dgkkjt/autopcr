e83381a46513fa032649bd72807db86f4f2907cf
czl <531040183@qq.com>
2025-10-23 00:47:14 +0800

    fix: enhance

--- autopcr/model/custom.py
+++ autopcr/model/custom.py
@@ -201,3 +201,10 @@ class ArenaQueryResponse(BaseModel):
 class TalentQuestData(BaseModel):
     talent_id: int = None
     clear_count: int = None
+
+class EffectiveUnitData(BaseModel):
+    unit_id: int = 0
+    unit_lv: int = 0
+    rank: int = 0
+    equip_num: int = 0
+    equip_enhance_num: int = 0
